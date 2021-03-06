7a240419e2eb48d223f4c6bf6c0a45845fea73bb
Jonas Hendrickx <jonas@yukisoftware.es>
2021-03-06 11:16:49 +0100

    Set backlight to on by default.

--- auto-keyboard-backlight/auto-keyboard-backlight.ps1
+++ auto-keyboard-backlight/auto-keyboard-backlight.ps1
@@ -1,6 +1,6 @@
 # Settings
 [string]$path = 'C:\ProgramData\Lenovo\ImController\Plugins\ThinkKeyboardPlugin\x86\';
-[int]$level = 0; # Backlight level: 0 - Off, 1 - Dim, 2 - On
+[int]$level = 2; # Backlight level: 0 - Off, 1 - Dim, 2 - On
 
 # Don't modify below
 [string]$core = $path + 'Keyboard_Core.dll';
